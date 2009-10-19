b95da745a8d1a664fc319aaead4a1021042a1b60
Jakub Wilk <jwilk@jwilk.net>
2009-10-19 09:08:39 +0200

    Bump version number.

--- lib/ocrodjvu.py
+++ lib/ocrodjvu.py
@@ -11,7 +11,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 # General Public License for more details.
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
 import inspect
 import optparse
