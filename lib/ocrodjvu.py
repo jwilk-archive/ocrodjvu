64c7166b684b883278caa350eee946ea0e1709c0
Jakub Wilk <jwilk@jwilk.net>
2009-11-27 21:14:27 +0100

    New version will be 0.3.0.

--- lib/ocrodjvu.py
+++ lib/ocrodjvu.py
@@ -11,7 +11,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 # General Public License for more details.
 
-__version__ = '0.2.2'
+__version__ = '0.3.0'
 
 import inspect
 import optparse
