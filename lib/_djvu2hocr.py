a502169c6c90b1e67649560e1848c2356612ff46
Jakub Wilk <jwilk@jwilk.net>
2010-01-02 18:22:43 +0100

    Update copyright information.

--- lib/_djvu2hocr.py
+++ lib/_djvu2hocr.py
@@ -1,5 +1,5 @@
 # encoding=UTF-8
-# Copyright © 2009 Jakub Wilk <ubanus@users.sf.net>
+# Copyright © 2009, 2010 Jakub Wilk <ubanus@users.sf.net>
 #
 # This package is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
