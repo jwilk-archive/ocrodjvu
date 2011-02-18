1902c847eac443a83a88c81580eb6d75fbf2b2c7
Jakub Wilk <jwilk@jwilk.net>
2011-02-18 19:49:32 +0100

    s/This package is/This program is/; add a newline between encoding declaration and license information.

--- lib/engines/cuneiform.py
+++ lib/engines/cuneiform.py
@@ -1,11 +1,12 @@
 # encoding=UTF-8
+
 # Copyright © 2010, 2011 Jakub Wilk <jwilk@jwilk.net>
 #
-# This package is free software; you can redistribute it and/or modify
+# This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; version 2 dated June, 1991.
 #
-# This package is distributed in the hope that it will be useful, but
+# This program is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 # General Public License for more details.
