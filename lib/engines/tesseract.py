595315efed0ad854671e1453ad1aa0f579abaacd
Jakub Wilk <jwilk@jwilk.net>
2011-08-18 12:34:05 +0200

    engines.tesseract: make Tesseract error message pattern less selective.

--- lib/engines/tesseract.py
+++ lib/engines/tesseract.py
@@ -33,7 +33,7 @@ from .. import utils
 const = text_zones.const
 
 _language_pattern = re.compile('^[a-z]{3}(-[a-z]+)?$')
-_error_pattern = re.compile(r"^(Error openn?ing data|Unable to load unicharset) file (?P<dir>/.*)/[.](?P<ext>[a-z]+)\n$", re.DOTALL)
+_error_pattern = re.compile(r"^(Error openn?ing data|Unable to load unicharset) file (?P<dir>/.*)/[.](?P<ext>[a-z]+)\n")
 
 _bbox_extras_template = '''\
 <!-- The following script was appended to hOCR by ocrodjvu -->
