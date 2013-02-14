7af8fc3de2cd3ac4fdc9b825f38d2016c638d988
Jakub Wilk <jwilk@jwilk.net>
2013-02-14 23:15:29 +0100

    Rephrase Python version compatibility error messages.

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,8 +1,8 @@
 import sys
 
 if sys.version_info < (2, 5):
-    raise RuntimeError('ocrodjvu requires Python >= 2.5')
+    raise RuntimeError('Python >= 2.5 is required')
 if sys.version_info >= (3, 0):
-    raise RuntimeError('ocrodjvu is not compatible with Python 3.X')
+    raise RuntimeError('Python 2.X is required')
 
 # vim:ts=4 sw=4 et
