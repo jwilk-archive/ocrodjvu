2f6abe961b9fdc545301ea855679937832af2cfc
Jakub Wilk <jwilk@jwilk.net>
2013-03-14 21:29:02 +0100

    cli.djvu2hocr: standardize on uppercase “UTF-8”.

--- lib/cli/djvu2hocr.py
+++ lib/cli/djvu2hocr.py
@@ -274,7 +274,7 @@ hocr_header_template = '''\
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
   <meta name="ocr-system" content="%(ocr_system)s" />
   <meta name="ocr-capabilities" content="%(ocr_capabilities)s" />
   <style type="text/css">%(css)s</style>
