61e6a545bd44e526e5c597594ca3fcfed96de702
Jakub Wilk <jwilk@jwilk.net>
2013-04-16 22:47:39 +0200

    Update copyright information.

--- lib/engines/common.py
+++ lib/engines/common.py
@@ -1,6 +1,6 @@
 # encoding=UTF-8
 
-# Copyright © 2010, 2011, 2012 Jakub Wilk <jwilk@jwilk.net>
+# Copyright © 2010, 2011, 2012, 2013 Jakub Wilk <jwilk@jwilk.net>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
