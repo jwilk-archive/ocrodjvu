e514817851425977233b2bda166bc0fa8b99710a
Jakub Wilk <jwilk@jwilk.net>
2013-04-17 00:24:32 +0200

    cli.ocrodjvu: move --ocr-only and --clear-text to the “options controlling output” group. doc/ocrodjvu: follow the above changes; improve --ocr-only documentation.

--- lib/cli/ocrodjvu.py
+++ lib/cli/ocrodjvu.py
@@ -182,7 +182,7 @@ class ArgumentParser(argparse.ArgumentParser):
         version = '%(prog)s ' + __version__
         argparse.ArgumentParser.__init__(self, usage=usage)
         self.add_argument('-v', '--version', action='version', version=version, help='show version information and exit')
-        saver_group = self.add_argument_group(title='options controlling output')
+        group = self.add_argument_group(title='options controlling output')
         for saver_type in self.savers:
             options = saver_type.options
             try:
@@ -193,7 +193,7 @@ class ArgumentParser(argparse.ArgumentParser):
             metavar = None
             if n_args == 1:
                 metavar = 'FILE'
-            saver_group.add_argument(
+            group.add_argument(
                 *options,
                 **dict(
                     metavar=metavar,
@@ -202,12 +202,12 @@ class ArgumentParser(argparse.ArgumentParser):
                     help=saver_type.__doc__
                 )
             )
-        saver_group.add_argument('--save-raw-ocr', dest='save_raw_ocr_dir', metavar='DIRECTORY', help='save raw OCR output')
-        saver_group.add_argument('--raw-ocr-filename-template', metavar='TEMPLATE', default='{id-ext}', help='file naming scheme for raw OCR')
+        group.add_argument('--ocr-only', dest='ocr_only', action='store_true', default=False, help='''don't save pages without OCR''')
+        group.add_argument('--clear-text', dest='clear_text', action='store_true', default=False, help='remove existing hidden text')
+        group.add_argument('--save-raw-ocr', dest='save_raw_ocr_dir', metavar='DIRECTORY', help='save raw OCR output')
+        group.add_argument('--raw-ocr-filename-template', metavar='TEMPLATE', default='{id-ext}', help='file naming scheme for raw OCR')
         self.add_argument('-e', '--engine', dest='engine', nargs=1, action=self.set_engine, default=self.default_engine, help='OCR engine to use')
         self.add_argument('--list-engines', action=self.list_engines, nargs=0, help='print list of available OCR engines')
-        self.add_argument('--ocr-only', dest='ocr_only', action='store_true', default=False, help='''don't save pages without OCR''')
-        self.add_argument('--clear-text', dest='clear_text', action='store_true', default=False, help='remove existing hidden text')
         self.add_argument('-l', '--language', dest='language', help='set recognition language')
         self.add_argument('--list-languages', action=self.list_languages, nargs=0, help='print list of available languages')
         self.add_argument('--render', dest='render_layers', choices=self._render_map.keys(), action='store', default='mask', help='image layers to render')
