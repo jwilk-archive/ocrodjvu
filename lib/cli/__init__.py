71dae40961e92aba7d9b72e4a88cfa8f680efdf5
Jakub Wilk <jwilk@jwilk.net>
2014-04-21 22:39:23 +0200

    Use HTTPS in enhance_import_error() calls.

--- lib/cli/__init__.py
+++ lib/cli/__init__.py
@@ -3,7 +3,7 @@ from .. import utils
 try:
     import argparse
 except ImportError, ex:
-    utils.enhance_import_error(ex, 'argparse', 'python-argparse', 'http://code.google.com/p/argparse/')
+    utils.enhance_import_error(ex, 'argparse', 'python-argparse', 'https://code.google.com/p/argparse/')
     raise
 
 argparse.ArgumentParser # quieten pyflakes
