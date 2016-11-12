06ba912a9dc5b752ada07f79ad90ca2d1ba8145f
Jakub Wilk <jwilk@jwilk.net>
2016-11-12 18:15:17 +0100

    Update copyright information.

--- lib/iso639.py
+++ lib/iso639.py
@@ -1,6 +1,6 @@
 # encoding=UTF-8
 
-# Copyright © 2013 Jakub Wilk <jwilk@jwilk.net>
+# Copyright © 2013-2016 Jakub Wilk <jwilk@jwilk.net>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
