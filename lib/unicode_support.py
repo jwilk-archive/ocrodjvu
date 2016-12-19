jwilk-archive/ocrodjvu

commit e050715739f3f74a41365ba6e87425c9cf68778e

Drop support for PyICU before 1.0.

lib/unicode_support.py
@@ -15,18 +15,11 @@ from . import utils
 
 def get_icu():
     try:
-        # For PyICU ≥ 1.0
         import icu
-        return icu
     except ImportError:  # <no-coverage>
-        pass
-    try:  # <no-coverage>
-        # For PyICU < 1.0
-        import PyICU as icu
-        return icu
-    except ImportError as ex:  # <no-coverage>
         utils.enhance_import_error(ex, 'PyICU', 'python-pyicu', 'https://pypi.python.org/pypi/PyICU/')
-        raise
+    else:
+        return icu
 
 def simple_word_break_iterator(text):
     '''

commit bea1b3fb5e27701542aa3e5ddb584ef69c118b7e

unicode_support: fix exception handling.
Fixes:

    NameError: global name 'ex' is not defined

when PyICU was not found.

Fixes: e050715739f3f74a41365ba6e87425c9cf68778e

lib/unicode_support.py
@@ -16,7 +16,7 @@ from . import utils
 def get_icu():
     try:
         import icu
-    except ImportError:  # no coverage
+    except ImportError as ex:  # no coverage
         utils.enhance_import_error(ex, 'PyICU', 'python-pyicu', 'https://pypi.python.org/pypi/PyICU/')
     else:
         return icu