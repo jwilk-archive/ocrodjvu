f371fd732b83283c2685b43ee92e2c9044310d4a
Jakub Wilk <jwilk@jwilk.net>
2017-01-25 13:02:19 +0100

    Update license notices headers.
    They are now more similar to those recommended by FSF:
    https://www.gnu.org/licenses/gpl-howto.en.html

--- lib/cli/__init__.py
+++ lib/cli/__init__.py
@@ -2,14 +2,16 @@
 
 # Copyright © 2011-2015 Jakub Wilk <jwilk@jwilk.net>
 #
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; version 2 dated June, 1991.
+# This file is part of ocrodjvu.
 #
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-# General Public License for more details.
+# ocrodjvu is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# ocrodjvu is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+# for more details.
 
 from .. import utils
 
