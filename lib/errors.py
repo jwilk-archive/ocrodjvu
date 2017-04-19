6874fe20eb7aaf82ece6f32e3a4b1eca3d6cc17d
Jakub Wilk <jwilk@jwilk.net>
2017-04-19 21:55:24 +0200

    errors: wrap long lines.

--- lib/errors.py
+++ lib/errors.py
@@ -16,32 +16,50 @@
 class UnknownLanguageList(Exception):
 
     def __init__(self):
-        Exception.__init__(self, 'unable to determine list of available languages')
+        Exception.__init__(self,
+            'unable to determine list of available languages'
+        )
 
 class InvalidLanguageId(ValueError):
 
     def __init__(self, name):
-        ValueError.__init__(self, 'invalid language identifier: {lang}; language identifier is typically an ISO 639-2 three-letter code'.format(lang=name))
+        ValueError.__init__(self,
+            'invalid language identifier: {lang}; '
+            'language identifier is typically an ISO 639-2 three-letter code'
+            .format(lang=name)
+        )
 
 class MissingLanguagePack(Exception):
 
     def __init__(self, language):
-        Exception.__init__(self, 'language pack for the selected language ({lang}) is not available'.format(lang=language))
+        Exception.__init__(self,
+            'language pack for the selected language ({lang}) is not available'
+            .format(lang=language)
+        )
 
 class EngineNotFound(Exception):
 
     def __init__(self, name):
-        Exception.__init__(self, 'OCR engine ({engine}) was not found'.format(engine=name))
+        Exception.__init__(self,
+            'OCR engine ({engine}) was not found'
+            .format(engine=name)
+        )
 
 class MalformedOcrOutput(Exception):
 
     def __init__(self, message):
-        Exception.__init__(self, 'malformed OCR output: {msg}'.format(msg=message))
+        Exception.__init__(self,
+            'malformed OCR output: {msg}'
+            .format(msg=message)
+        )
 
 class MalformedHocr(MalformedOcrOutput):
 
     def __init__(self, message):
-        Exception.__init__(self, 'malformed hOCR document: {msg}'.format(msg=message))
+        Exception.__init__(self,
+            'malformed hOCR document: {msg}'
+            .format(msg=message)
+        )
 
 EXIT_FATAL = 1
 EXIT_NONFATAL = 2
