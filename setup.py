jwilk-archive/ocrodjvu

commit 34d5480c2923be8403f3bfa987e37a196ebaefb0

setup: get version from changelog.

setup.py
@@ -36,7 +36,10 @@ except ImportError:
 else:
     distutils644.install()
 
-from lib import version
+def get_version():
+    with open('doc/changelog', 'r') as file:
+        line = file.readline()
+    return line.split()[1].strip('()')
 
 classifiers = '''
 Development Status :: 4 - Beta
@@ -179,7 +182,7 @@ else:
 
 distutils.core.setup(
     name='ocrodjvu',
-    version=version.__version__,
+    version=get_version(),
     license='GNU GPL 2',
     description='OCR for DjVu',
     long_description=__doc__.strip(),

commit 2a24961ba86638f5dfc93cca5f1b62ab658d8a03

setup: add Python 3 version check.
Fixes: 34d5480c2923be8403f3bfa987e37a196ebaefb0

setup.py
@@ -18,6 +18,7 @@
 '''
 
 b''  # Python >= 2.6 is required
+exec b''  # Python 2.X is required
 
 import glob
 import os