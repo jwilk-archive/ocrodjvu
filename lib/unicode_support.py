jwilk-archive/ocrodjvu

commit e050715739f3f74a41365ba6e87425c9cf68778e

Drop support for PyICU before 1.0.

lib/unicode_support.py
@@ -15,18 +15,11 @@ from . import utils
 
 def get_icu():
     try:
-        # For PyICU ≥ 1.0
         import icu
-        return icu
     except ImportError:  # <no-coverage>
-        pass
-    try:  # <no-coverage>
-        # For PyICU < 1.0
-        import PyICU as icu
-        return icu
-    except ImportError as ex:  # <no-coverage>
         utils.enhance_import_error(ex, 'PyICU', 'python-pyicu', 'https://pypi.python.org/pypi/PyICU/')
-        raise
+    else:
+        return icu
 
 def simple_word_break_iterator(text):
     '''

commit d0178cec74fa261a754ad329302d202b355b4eb1

unicode_support: fix error handling for missing PyICU.
Fixes: e050715739f3f74a41365ba6e87425c9cf68778e

Fixes:

    Traceback (most recent call last):
      ...
      File ".../ocrodjvu/lib/version.py", line 65, in __call__
        print('+ PyICU {0}'.format(pyicu.VERSION))
    AttributeError: 'NoneType' object has no attribute 'VERSION'

lib/unicode_support.py
@@ -20,6 +20,7 @@ def get_icu():
         import icu
     except ImportError as ex:  # no coverage
         utils.enhance_import_error(ex, 'PyICU', 'python-pyicu', 'https://pypi.org/project/PyICU/')
+        raise
     else:
         return icu
 